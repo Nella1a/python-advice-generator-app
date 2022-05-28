dd0ab2a3aac3408deb7b280a8b6ef88b3c22dc83
Nella1a <m.allen@gmx.at>
2022-05-28 21:02:17 +0200

    Fix output message

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import requests
 
 res_object = requests.get("https://api.adviceslip.com/advice")
 response = res_object.json()
-print(response)
-print(response["slip"]["advice"])
+#print(response)
+
+print(f'****** ADVICE {response["slip"]["id"]} ******')
+print(f'"{response["slip"]["advice"]}"\n')
